2b55e72113101c73f7f7b1dadb1434297d8524da
emreturgutce <emrebursalee@gmail.com>
2020-10-02 22:44:22 +0300

    text changed in app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          I am learning CI/CD
         </a>
       </header>
     </div>
